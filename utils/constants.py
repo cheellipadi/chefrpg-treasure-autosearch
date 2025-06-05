c87532c720bbd462ece7bb0cb56c6ff8119d7b19
cheeyang <tehcheeyang@gmail.com>
2025-06-05 10:38:39 +0800

    chore: set default target chests to epic and legendary

--- utils/constants.py
+++ utils/constants.py
@@ -6,7 +6,8 @@ WAIT_AFTER_LOAD = 4 # Seconds to wait after loading (before starting the movemen
 # WALK_PATTERN e.g. [[1,1],[-1,0]] -> walk diagonally up and right for 1s, then left for 1s
 WALK_PATTERN = [[0.9, -0.8],[0,-0.4],[-0.9,-1.8]]
 # DISPLAY_SCALE_FACTOR = 2 # Usually 2 for retina displays, 1 for normal displays
-TARGET_CHESTS = ['EPIC','LEGENDARY','COMMON','RARE'] # Example supported values: ['LEGENDARY'], ['EPIC'], ['LEGENDARY','EPIC'], ['EPIC','LEGENDARY','COMMON','RARE']
+TARGET_CHESTS = ['EPIC','LEGENDARY'] # Example supported values: ['LEGENDARY'], ['EPIC'], ['LEGENDARY','EPIC'], ['EPIC','LEGENDARY','COMMON','RARE']
+
 # Auto-detect display scale factor (macOS only)
 try:
     from AppKit import NSScreen
